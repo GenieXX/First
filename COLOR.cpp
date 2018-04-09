caf1fc9edb5eb7e17c110deee5826c3d018c8aa2
GenieXX <38209947+GenieXX@users.noreply.github.com>
2018-04-09 18:58:26 +0800

    Add files via upload

---
+++ COLOR.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "COLOR.h"
+
+
+Color::Color() {
+	red = 0;
+	blue = 0;
+	green = 0;
+
+}
+
+Color::Color(int cred, int cgreen, int cblue) {
+	red = cred;
+	blue = cblue;
+	green = cgreen;
+
+}
